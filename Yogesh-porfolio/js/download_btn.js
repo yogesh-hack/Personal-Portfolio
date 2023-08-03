ee8c93e4a3cf2adf8c3370095492742e9ff3e83b
YOGESH BAGHEL <83384315+yogesh-hack@users.noreply.github.com>
2023-08-03 11:34:17 +0530

    Create download_btn.js

---
+++ Yogesh-porfolio/js/download_btn.js
@@ -0,0 +1,27 @@
+const downloadBtn = document.querySelector(".download-btn");
+const fileLink = "https://drive.google.com/file/d/1dHE9keBlpRRvH498Ng4onEyKVRcHDTzP/view?usp=sharing";
+
+const initTimer = () => {
+    if(downloadBtn.classList.contains("disable-timer")) {
+        return location.href = fileLink;
+    }
+    let timer = downloadBtn.dataset.timer;
+    downloadBtn.classList.add("timer");
+    downloadBtn.innerHTML = `Resume download will begin in <b>${timer}</b> seconds`;
+    const initCounter = setInterval(() => {
+        if(timer > 0) {
+            timer--;
+            return downloadBtn.innerHTML = `Resume download will begin in <b>${timer}</b> seconds`;
+        }
+        clearInterval(initCounter);
+        location.href = fileLink;
+        downloadBtn.innerText = "Resume is downloading...";
+        setTimeout(() => {
+            downloadBtn.classList.replace("timer", "disable-timer");
+            downloadBtn.innerHTML = `<span><img src="https://www.svgrepo.com/show/140007/download-button.svg" width="20px" height="20px"></span>
+                                    <span class="text">Download Resume</span>`;
+        }, 3000);
+    }, 1000);
+}
+
+downloadBtn.addEventListener("click", initTimer);
